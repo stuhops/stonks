510ad94bd71d57d24b16fa992e3c429c0dbf0b87
Stuart Hopkins <stwhop@gmail.com>
2021-04-01 23:06:14 -0600

    name change

--- analyze.py
+++ analyze.py
@@ -1,7 +1,7 @@
 import json
 import pathlib
 from AlpacaTrade import AlpacaTrade
-from TradingAlgorithms import TradingAlgorithms
+from TradingAlgorithms import MeanReversion
 
 # Get the parent directories path so we don't have to hardcode
 path = pathlib.Path().cwd()
@@ -20,20 +20,20 @@ def float_range(start=0, stop=1, step=1):
 
 # Setup the dictionary with tickers and price lists
 tickers = {
-    # "SINT": {"file_name": "SINT_3_year.txt"},
     "SINT": {},
+    "VISL": {},
 }
 
-diff_range = list(float_range(-20, 20, 0.1))
+diff_range = list(float_range(-20, 20, 0.5))
 
 for ticker in tickers:
-    prices = AlpacaTrade.get_historical_data(ticker, limit=10, to_return={"c"})
-    best_days = TradingAlgorithms.mean_reversion_best_settings(
+    prices = AlpacaTrade.get_historical_data(ticker, limit=400, to_return={"c"})
+    best_days = MeanReversion.get_best_settings(
         prices,
         num_best=10,
         diff_range=diff_range,
         day_range=range(1, 10),
-        # data_splits=range(10),
+        data_splits=range(10),
     )
 
     tickers[ticker]["best_days"] = best_days
